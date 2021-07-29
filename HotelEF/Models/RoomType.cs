4938a7fae36e15fdcc5a2726f43a3952e9713656
paula <softapps680@gmail.com>
2021-07-29 22:23:13 +0200

    .

--- HotelEF/Models/RoomType.cs
+++ HotelEF/Models/RoomType.cs
@@ -11,7 +11,7 @@ namespace HotelUWP.Models
         public int Id { get; set; }
         public string RoomTypeName { get; set; }
         public decimal Price { get; set; }
-        //public virtual Room Room { get; set; }
+       
 
     }
 }
